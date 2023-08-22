10c0d91144951a8294b9bd81fa86f8877f5f1342
Robson-Bitencourt <142269406+Robson-Bitencourt@users.noreply.github.com>
2023-08-22 18:42:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function carConteudo1(){
+    document.getElementById(carConteudo).innerHTML=<p>A internet teve suas origens na década de 1960 como uma rede de computadores do
+    Departamento de Defesa dos EUA, chamada ARPANET (Advanced Research Projects Agency
+    Network). Originalmente, a ARPANET foi criada para permitir que cientistas e acadêmicos de
+    diferentes locais físicos compartilhassem informações e colaborassem em pesquisa.</p>
+}
+function carConteudo2(){
+    document.getElementById(carConteudo).innerHTML = <p>"Os anos 70 testemunharam o desenvolvimento dos protocolos TCP/IP (Transmission Control
+        Protocol/Internet Protocol), que permitem a transferência de dados entre computadores em
+        redes diferentes. A adoção desses protocolos pela ARPANET em 1983 marcou a
+        operacionalização da internet como a conhecemos hoje.
+    "</p>
+}
+function carConteudo3(){
+    document.getElementById(carConteudo).innerHTML = <p>Durante os anos 80, a internet começou a se expandir além do uso acadêmico e militar,
+    entrando no domínio comercial. Em 1989, a NSF (National Science Foundation) permitiu o
+    uso comercial da internet, resultando em seu rápido crescimento.</p>
+}
+function carConteudo4(){
+    document.getElementById(carConteudo).innerHTML = <p>Em 1991, o CERN (Conselho Europeu para a Pesquisa Nuclear) apresentou ao mundo a World Wide Web, uma rede de hipertexto
+    acessível ao público e projetada para tornar a internet mais fácil de usar. O primeiro navegador web, chamado Mosaic, foi lançado
+    em 1993, tornando a Web mais acessível para o usuário comum.
+    No final dos anos 1990, a internet deu lugar à "bolha do ponto com", uma época de especulação de mercado em torno das
+    empresas de internet. Embora muitas dessas empresas não tenham sobrevivido ao estouro da bolha em 2000-2002, outras, como
+    Amazon e Google, não apenas sobreviveram, mas se tornaram algumas das maiores empresas do mundo.</p>
+}
+function carConteudo5(){
+    document.getElementById(carConteudo).innerHTML = <p>Os anos 2000 foram caracterizados pelo surgimento das redes sociais e do compartilhamento de vídeos online. Sites como
+    Facebook, YouTube e Twitter foram lançados durante esse período, e o acesso à internet em dispositivos móveis começou a se
+    tornar cada vez mais comum.</p>
+}
